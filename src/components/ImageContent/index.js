7b741486529ed776c659056089909fffcd92fead
Wendell Fowlkes III <123840237+AWindyAC@users.noreply.github.com>
2023-10-15 17:31:50 -0400

    Create index.js

---
+++ src/components/ImageContent/index.js
@@ -0,0 +1,18 @@
+//Using this to handle the multiple imports of images I will need to do
+import team1 from '../../assets/Oviedo2.jpg'
+import team2 from '../../assets/Oviedo4.jpg'
+import team3 from '../../assets/morgan_ben.jpg';
+
+//Home page gallery images
+export const tyeDyeDay = require('../../assets/Oviedo2.jpg');
+export const tyeDyeDay2 = require('../../assets/Oviedo4.jpg');
+export const morgan_and_ben = require('../../assets/morgan_ben.jpg');
+export const joe_charles_jacob = require('../../assets/BOH.jpg');
+export const directorCruise = require('../../assets/director_cruise.jpg');
+export const motherSonDateNight = require('../../assets/Duaghter_Son_Knight.jpg');
+export const georgeBirthday = require('../../assets/george_bday.jpg');
+export const easter = require('../../assets/easter.jpg');
+export const servers = require('../../assets/servers.jpg');
+export const universal = require('../../assets/leadership_universal.jpg');
+//Meet the team default image
+export const profilePicDefault = require('../../assets/profile-pic.jpg');
