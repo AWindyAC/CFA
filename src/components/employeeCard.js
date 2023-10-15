bd39b8fa446c6f2c7889c30bc4ef93bf3a4ce283
Wendell Fowlkes III <123840237+AWindyAC@users.noreply.github.com>
2023-10-15 10:52:07 -0400

    Update employeeCard.js
    Added data from API and conditional rendering for employee department.

--- src/components/employeeCard.js
+++ src/components/employeeCard.js
@@ -5,11 +5,10 @@ function EmployeeCard() {
     <div>
         <div className='EmployeeCard' style={{margin:'1rem', alignItems:'center'}}>
             <img className= 'employee-pic' src=""/>
-            <h3 style={{color:'white'}}></h3>
-            <span class="label label-1">Director</span>
-            <span class="label label-2">FOH</span>
-            <span class="label label-3">BOH</span>
-            <p style={{color:'white'}}>!Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam accusamus iure quae deleniti veniam possimus</p>
+          <h3 style={{color:'white'}}>{employeeData.name}</h3>
+          <span class="label label-1">{employeeData.title}</span><br></br>
+          <span class="label label-2">{employeeData.department[0]}</span>
+          {employeeData.department[1] ? <span class="label label-3">{employeeData.department[1]}</span> : <></> }
         </div>
     </div>
   )
