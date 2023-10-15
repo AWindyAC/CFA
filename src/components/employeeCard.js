0f8b099e827e7897afabc9d16413945635083a87
Wendell Fowlkes <fowlkes.wendell@gmail.com>
2023-10-15 08:55:31 -0400

    Airtable API working

--- src/components/employeeCard.js
+++ src/components/employeeCard.js
@@ -5,8 +5,12 @@ const EmployeeCard = ({ employeeData }) => {
     <div>
         <div className='EmployeeCard' style={{margin:'1rem', alignItems:'center'}}>
             <img className= 'employee-pic' src=""/>
+            <img
+              className="employee-pic"
+              src={employeeData.photo}
+            />
             <h3 style={{color:'white'}}>{employeeData.name}</h3>
-            <span class="label label-1">Director</span>
+            <span class="label label-1">{employeeData.title}</span>
             <span class="label label-2">FOH</span>
             <span class="label label-3">BOH</span>
             <p style={{color:'white'}}>!Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam accusamus iure quae deleniti veniam possimus</p>
