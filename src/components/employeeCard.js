3359b97de912544d4ab3ee581665692910843d0d
Wendell Fowlkes III <123840237+AWindyAC@users.noreply.github.com>
2023-10-15 17:30:11 -0400

    Update employeeCard.js

--- src/components/employeeCard.js
+++ src/components/employeeCard.js
@@ -1,16 +1,18 @@
 import React from 'react'
+import { profilePicDefault } from './ImageContent'
 
-function EmployeeCard() {
+const EmployeeCard = ({ employeeData }) => {
   return (
-    <div>
-        <div className='EmployeeCard' style={{margin:'1rem', alignItems:'center'}}>
-            <img className= 'employee-pic' src=""/>
+      <div className='EmployeeCard' style={{margin:'1rem', alignItems:'center'}}>
+          <img
+            className="employee-pic"
+            src={profilePicDefault}
+          />
           <h3 style={{color:'white'}}>{employeeData.name}</h3>
           <span class="label label-1">{employeeData.title}</span><br></br>
           <span class="label label-2">{employeeData.department[0]}</span>
           {employeeData.department[1] ? <span class="label label-3">{employeeData.department[1]}</span> : <></> }
-        </div>
-    </div>
+      </div>
   )
 }
 
