633e6a8e04d33b0fd2187f850c177b8434507ac0
Wendell Fowlkes III <123840237+AWindyAC@users.noreply.github.com>
2023-10-15 17:27:59 -0400

    Update pathForGrowth.js

--- src/components/pathForGrowth.js
+++ src/components/pathForGrowth.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 function PathForGrowth() {
   return (
-    <div>
+    <div className='bg-white text-grey text-center'>
       <h1>Path For Growth</h1>
+      <div className='container-fluid growth-container'>
+      <div className='col-xl-6 growthDiv'><h3>Trainee</h3></div>
+      <div className='col-xl-6 growthDiv'><h3>Team Member</h3></div>
+      <br></br>
+      <div className='col-xl-6 growthDiv'><h3>Trainer</h3></div>
+      <div className='col-xl-6 growthDiv'><h3>Service Leader</h3></div>
+
+      <div className='col-xl-6 growthDiv'><h3>Crew Leader</h3></div>
+      <div className='col-xl-6 growthDiv'><h3>Director</h3></div>
+      </div>
     </div>
   )
 }
