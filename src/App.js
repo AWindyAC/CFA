146147ba1f17695b48d4e748784ddf297907d3cb
Wendell Fowlkes <fowlkes.wendell@gmail.com>
2023-11-25 14:15:40 -0500

    Recent changes

--- src/App.js
+++ src/App.js
@@ -8,21 +8,21 @@ import Employees from "./components/employees";
 import Events from "./components/events";
 import PathForGrowth from "./components/pathForGrowth";
 import Uniforms from "./components/uniforms";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, HashRouter } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-        <Routes>
+        <HashRouter>
           <Route exact path='/' element={<Hero />} />
           <Route path='/about' element={<About />} />
           <Route path='/meet-the-team' element={<Employees />} />
           <Route path='/events' element={<Events />} />
           <Route path='/path-for-growth' element={<PathForGrowth />} />
           <Route path='/uniforms' element={<Uniforms />} />
-        </Routes>
+        </HashRouter>
         <Footer />
       </Router>
     </>
